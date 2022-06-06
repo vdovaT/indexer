98bb512f55a262f17663b456f324c6e589d45f93
Ofir Sudai <ofirsudai@Ofirs-MacBook-Pro.local>
2022-06-06 17:41:20 -0400

    feat: moved logic to postgres

--- src/jobs/s3-export/data-sources/index.ts
+++
@@ -1,8 +0,0 @@
-export abstract class BaseDataSource {
-  public abstract getData(cursor: string | null, limit: number): Promise<GetDataResult>;
-}
-
-export type GetDataResult = {
-  data: Record<string, unknown>[];
-  nextCursor: string | null;
-};
